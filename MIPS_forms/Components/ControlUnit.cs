4e28061bd727f0c2411148f387377441416c8cfa
robibubble27 <robert.1709@yahoo.com>
2022-12-20 20:05:38 +0200

    Completed control unit

--- MIPS_forms/Components/ControlUnit.cs
+++ MIPS_forms/Components/ControlUnit.cs
@@ -45,11 +45,66 @@ namespace MIPS_forms.Components
                 //instructiune de tip r
                 case 0:
                     OutPorts["regDst"] = 1;
-                    OutPorts["ALUOp"] = 10;
-                    OutPorts["ALUSrc"] = 0;
+                    OutPorts["ALUOp"] = 0;
                     OutPorts["regWrite"] = 1;
                     break;
-                
+                //addi
+                case 1: //extop posibil redundant
+                    OutPorts["extOp"] = 1;
+                    OutPorts["ALUSrc"] = 1;
+                    OutPorts["regWrite"] = 1;
+                    OutPorts["ALUOp"] = 1;
+                    break;
+                //lw
+                case 2:
+                    OutPorts["extOp"] = 1;
+                    OutPorts["ALUSrc"] = 1;
+                    OutPorts["memToReg"] = 1;
+                    OutPorts["regWrite"] = 1;
+                    OutPorts["ALUOp"] = 1;
+                    break;
+                //sw
+                case 3:
+                    OutPorts["extOp"] = 1;
+                    OutPorts["ALUSrc"] = 1;
+                    OutPorts["memWrite"] = 1;
+                    OutPorts["regWrite"] = 1;
+                    OutPorts["ALUOp"] = 1;
+                    break;
+                //beq
+                case 4:
+                    OutPorts["extOp"] = 1;
+                    OutPorts["branch"] = 1;
+                    OutPorts["ALUOp"] = 2;
+                    break;
+                //andi
+                case 5:
+                    OutPorts["ALUSrc"] = 1;
+                    OutPorts["regWrite"] = 1;
+                    OutPorts["ALUOp"] = 5;
+                    break;
+                //ori
+                case 6:
+                    OutPorts["ALUSrc"] = 1;
+                    OutPorts["regWrite"] = 1;
+                    OutPorts["ALUOp"] = 6;
+                    break;
+                //jump
+                case 7:
+                    OutPorts["jump"] = 1;
+                    break;
+
+                default:
+                    OutPorts["jump"] = 0;
+                    OutPorts["memToReg"] = 0;
+                    OutPorts["regWrite"] = 0;
+                    OutPorts["memWrite"] = 0;
+                    OutPorts["branch"] = 0;
+                    OutPorts["ALUOp"] = 0;
+                    OutPorts["ALUSrc"] = 0;
+                    OutPorts["regDst"] = 0;
+                    OutPorts["extOp"] = 0;
+                    break;
 
             }
             //send signals to other components here
